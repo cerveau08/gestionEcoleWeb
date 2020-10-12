af79498926155ea3a5375a6afa0162db5ffb467d
cerveau08 <malickcoly342@gmail.com>
2020-10-12 09:38:54 +0000

    dbmysql

--- .history/config/database_20201012093755.php
+++ .history/config/database_20201012093755.php
@@ -52,7 +52,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'gestionecoleweb'),
             'username' => env('DB_USERNAME', 'colycerv'),
-            'password' => env('DB_PASSWORD', 'cerveau08'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
